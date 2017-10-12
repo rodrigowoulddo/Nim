b24abbcd651d22e60d5f7d79a026542b016f7c45
Rodrigo <rodrigofgiglio@gmail.com>
2017-10-13 00:40:46 +0100

    - board size now can be changed at config

--- WebContent/javascript_files/configMethods.js
+++ WebContent/javascript_files/configMethods.js
@@ -17,3 +17,40 @@ btnConfig.onclick = function() {
 btnCloseConfig.onclick = function() {
     dialogConfig.style.display = "none";
 }
+
+
+/*Defines the clicks that change the board size*/
+var radio3piles = document.getElementById('radio3piles');
+var radio4piles = document.getElementById('radio4piles');
+var radio5piles = document.getElementById('radio5piles');
+
+var pile1 = document.getElementById("pile1");
+var pile2 = document.getElementById("pile2");
+var pile3 = document.getElementById("pile3");
+var pile4 = document.getElementById("pile4");
+var pile5 = document.getElementById("pile5");
+
+radio3piles.onclick = function () {
+    pile3.style.display= 'block';
+    pile1.style.display= 'block';
+    pile2.style.display= 'block';
+    pile4.style.display= 'none';
+    pile5.style.display= 'none';
+}
+
+radio4piles.onclick = function () {
+    pile1.style.display= 'block';
+    pile2.style.display= 'block';
+    pile3.style.display= 'block';
+    pile4.style.display= 'block';
+    pile5.style.display= 'none';
+}
+
+radio5piles.onclick = function () {
+    pile1.style.display= 'block';
+    pile2.style.display= 'block';
+    pile3.style.display= 'block';
+    pile4.style.display= 'block';
+    pile5.style.display= 'block';
+}
+
